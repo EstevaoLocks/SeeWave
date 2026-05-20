00258b8eb38acd7da63639950c66843794245174
estevaolocks <estevao.locks11@gmail.com>
2026-05-20 10:49:06 -0300

    salvando

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './assets/css/style.css'
+import { Header } from './Components/Header'
+import { Navbar } from './Components/Navbar'
+import './../assets/css/reset.css'
+import './../assets/css/style.css'
 
 function App() {
 
   return (
     <>
+      <Navbar />
+      <Header />
     </>
   )
 }
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,19 @@
+export function Navbar() {
+
+  return (
+    <nav>
+        <div>
+            <h1>Beach Buy</h1>
+        </div>
+        <ul>
+            <li>INÍCIO</li>
+            <li>EQUIPAMENTOS</li>
+            <li>AULAS</li>
+            <li>GALERIA</li>
+            <li>SOBRE</li>
+            <li>CONTATO</li>
+        </ul>
+        <div></div>
+    </nav>
+  )
+}
